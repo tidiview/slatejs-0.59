5d88520c206fee8557c1bf1083480dd15e37e5cd
François VIDIT <tidiview@users.noreply.github.com>
2020-11-23 12:36:23 +0100

    Initial commit

---
+++ src/Editor.js
@@ -0,0 +1,42 @@
+// Import React dependencies.
+import React, { useEffect, useMemo, useState } from "react";
+// Import the Slate editor factory.
+import { createEditor, Transforms } from "slate";
+
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from "slate-react";
+
+const App = () => {
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const [value, setValue] = useState([
+    {
+      type: "paragraph",
+      children: [{ text: "A line of text in a paragraph." }]
+    }
+  ]);
+
+  return (
+    // Add the editable component inside the context.
+    <Slate
+      editor={editor}
+      value={value}
+      onChange={(value) => {
+        console.log(value);
+        setValue(value);
+      }}
+    >
+      <Editable
+        onKeyDown={(event) => {
+          if (event.key === "Enter") {
+            event.preventDefault();
+            Transforms.insertText(editor, `\n`);
+            // add a white sapce
+            // Transforms.insertText(editor, `\n\u2060`);
+          }
+        }}
+      />
+    </Slate>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Editor from "./Editor";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Editor />, rootElement);
